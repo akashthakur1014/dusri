b9645b68b8003af1466d7be0ec09abb999ca9991
akashthakur1014 <akashlodhi1014@gmail.com>
2025-08-26 21:12:12 +0530

    Add files via upload

---
+++ assets/profile.js
@@ -0,0 +1,8 @@
+let btn = document.querySelector("#button1");
+
+btn.addEventListener("click",()=>{
+
+    let target = document.querySelector(".middiv").scrollIntoView({behavior:"smooth"});
+});
+
+
